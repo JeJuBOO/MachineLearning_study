60ea71a75404bb3ad1fda4d46a4ed5889977de01
qntjrrud11 <qntjrrud11@naver.com>
2021-04-27 16:41:37 +0900

    .

--- c3_matlab_perceptrone/backpropagation.m
+++ c3_matlab_perceptrone/backpropagation.m
@@ -47,7 +47,7 @@ while 1
 end
 
 fprintf("출력 벡터 : %5.4f %5.4f %5.4f %5.4f\n",o)
-fprintf("오차 : %5.4d\n세대 : %6.0f",mse(Y,o),epo)
+fprintf("오차 : %5.4d\n세대 : %6.0f\n",mse(Y,o),epo)
 plot((1:epo),error)
 
 %objective function
