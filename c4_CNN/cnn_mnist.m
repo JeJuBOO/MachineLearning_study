c6c826ee21dd394ec953676dad764d3ece40e8be
qntjrrud11 <qntjrrud11@naver.com>
2021-07-01 13:08:24 +0900

    [21.07.01] CNN / MNIST Version_1

--- c4_CNN/cnn_mnist.m
+++ c4_CNN/cnn_mnist.m
@@ -1,6 +1,6 @@
 clc,clear,close all
 % CNN / MNIST
-% Revision date: 2021.05.19
+% Revision date: 2021.07.01
 % mnist\trainingData.mat
 % mnist\trainingData.mat
 % images : 28 x 28 x 60000 
@@ -16,81 +16,108 @@ y = one_hot(labels+1,:)';
 % x(input images) : 28*28*1 60000
 % y(label) : 10*60000
 
-% % 입력데이터 정규화
-x = (x-mean(x,[1,2,3]))./std(x,0,[1,2,3]);
-x(isnan(x)) = 0;
+% 커널 크기
+U1_ab = [3 3 1 1];
+U2_cd = [4 4 1 1];
+U3_rq = [size(y,1) 25];
+
+%풀링 사이즈
+p1 = 2;
+p2 = 2;
+
+% 입력데이터 정규화
+x(isnan(Normalization3(x))) = 0;
 % 학습률
 lr = 0.1;
 
 %세대 수 (epoch)
-epo = 4;
+epo = 5000;
 
 %배치 수 
-batch = 1;
+batch = 100;
+
+
 
 % 커널
-kn_1 = randn(3,3,1,8);
-kn_2 = randn(3,3,1,16);
-kn_3 = randn(3,3,1,32);
-u = randn(10,7*7*128*32);
+U1 = randn(U1_ab(1),U1_ab(2),U1_ab(3),U1_ab(4)); % a*b
+U2 = randn(U2_cd(1),U2_cd(2),U2_cd(3),U2_cd(4)); % c*d
+U3 = randn(U3_rq(1),U3_rq(2));  % r*q
 
 %%
 for z = 1:epo
     % data shuffle
     p = randperm(length(x));
-    X = x(:,:,1,p(1:batch));
+    X = x(:,:,1,p(1:batch)); %i*j
     Y = y(:,p(1:batch));
     
-    dkn_1 = zeros(size(kn_1));
-    dkn_2 = zeros(size(kn_2));
-    dkn_3 = zeros(size(kn_3));
-    du = zeros(size(u));
+    dU1 = zeros(size(U1));% a*b
+    dU2 = zeros(size(U2));% c*d
+    dU3 = zeros(size(U3));% r*q
+    
     for i = 1 : batch
-        %Forward(in,kernel,act_func,stride,padding)
+        %Forward(in,kernel,stride,padding)
         %Pooling(in,num,stride)
-        layer1 = Forward(X(:,:,1,i),kn_1,@Relu,1,1);
-        pol_layer1 = Pooling(layer1,2,2);
+        X(:,:,1,i) = Normalization3(X(:,:,1,i));
+        z1 = Correlation(X(:,:,1,i),U1);
+        z1 = Normalization3(z1);
+        layer1 = Relu(z1);% m*n
+        pol_layer1 = Pooling(layer1,p1,p1); % m'*n'
         
-        layer2 = Forward(pol_layer1,kn_2,@Relu,1,1);
-        pol_layer2 = Pooling(layer2,2,2);
-        
-        layer3 = Forward(pol_layer2,kn_3,@Relu,1,1);
-
-        out_layer = FC(layer3,u);
+        z2 = Correlation(pol_layer1,U2);
+        z2 = Normalization3(z2);
+        layer2 = Relu(z2);% o*p
+        pol_layer2 = Pooling(layer2,p2,p2); % o'*p'
+       
+        flat_layer3 = reshape(pol_layer2,[],1); % q*1
+        out_layer = FC(flat_layer3,U3); % r*1
         out_layer = (out_layer-mean(out_layer))./std(out_layer);
-        o = Softmax(out_layer);
+        out = Softmax(out_layer);
+        error(i,:) = -sum(Y(:,i).*log(out));
+        %% back prop
+        gradient3 = Y(:,i) - out;
+        %출력층 gradient3
+        %FC층 U3
+        gradient2 = Gradient(layer2,z2,gradient3,U3,p2);% o*p
+        gradient1 = Gradient(layer1,z1,gradient2,U2,p1);% m*n
         
-        
-        gradient1 = o-Y(:,i);
-        gradient2 = reshape(gradient1'*u,size(layer3)).*ReluGradient(layer3);
-        gradient3 = gradient2'*kn_3.*ReluGradient(layer2);
-        du = du - gradient1*reshape(layer3,[],1)';
-        
-        
-        dkn_3 = dkn_3 - gradient2*u';
-        gradient3 = (gradient2'*U2(:,2:end))'.*Forward(@d_Sigmoid,X_sample(:,i),U1);
-        dU1 = dU1 -gradient3*X_sample(:,i)';
-        
-        
+        dU3 = dU3 - gradient3*flat_layer3';
+        dU2 = dU2 - Correlation(pol_layer1,gradient2);
+        dU1 = dU1 - Correlation(X(:,:,1,i),gradient1);
     end
+    U3 = U3 - lr*dU3/batch;
+    U2 = U2 - lr*dU2/batch;
+    U1 = U1 - lr*dU1/batch;
+    
+   tex1 = mean(error);
+   fprintf("전체 학습 오차: %0.5f\n",round(tex1,4))
 end
 
+load mnist\testingData.mat;
+X = reshape(images, [28,28,1,10000]);
+one_hot = diag(ones(1,max(labels)+1));
+Y = one_hot(labels+1,:)';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i=1:length(X)
+    %forward
+        X(:,:,1,i) = Normalization3(X(:,:,1,i));
+        z1 = Correlation(X(:,:,1,i),U1);
+        z1 = Normalization3(z1);
+        layer1 = Relu(z1);% m*n
+        pol_layer1 = Pooling(layer1,p1,p1); % m'*n'
+        
+        z2 = Correlation(pol_layer1,U2);
+        z2 = Normalization3(z2);
+        layer2 = Relu(z2);% o*p
+        pol_layer2 = Pooling(layer2,p2,p2); % o'*p'
+       
+        flat_layer3 = reshape(pol_layer2,[],1); % q*1
+        out_layer = FC(flat_layer3,U3); % r*1
+        out_layer = (out_layer-mean(out_layer))./std(out_layer);
+        out = Softmax(out_layer);
+        results(i) = min( Y(:,i) == (out(:,i) == max(out)));
+end
+error_epo = round(mean(results)*100,2);
+fprintf("\n정확도 : %5.4f\n",error_epo)
 
 
 
