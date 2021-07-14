496419de62c590f4a7c82525edd8ef39f62bd40f
qntjrrud11 <qntjrrud11@naver.com>
2021-07-14 17:33:50 +0900

    [21.07.14] adam / :( bad

--- c4_CNN/cnn_mnist.m
+++ c4_CNN/cnn_mnist.m
@@ -39,10 +39,11 @@ layerDim3 = layerDim2^2*kernelSize2(4);
 kernelSize3 = [labelClasses layerDim3];
 
 % 학습률
-lr = 0.01;
+lr = 0.1;
 %weight decay
 lambda = 0.0001;
-alpha_m = 0.9;
+alpha_m = 0.5;
+alpha_r = 0.9;
 
 %세대 수 (epoch)
 epo = 5;
@@ -64,14 +65,20 @@ v3 = zeros(size(U3));% r*q
 vb1 = zeros(size(B1));
 vb2 = zeros(size(B2));
 vb3 = zeros(size(B3));
-
+r1 = zeros(size(U1));% a*b
+r2 = zeros(size(U2));% c*d
+r3 = zeros(size(U3));% r*q
+rb1 = zeros(size(B1));
+rb2 = zeros(size(B2));
+rb3 = zeros(size(B3));
+    idx=0;
 tic
 %%
 for e = 1:epo
     % data shuffle
     p = randperm(length(x));
     
-    idx=0;
+
     for i = 1:batch:(length(x)-batch+1)
         %Correlation(in,kernel,stride,padding)
         %Pooling(in,num,stride)
@@ -79,6 +86,11 @@ for e = 1:epo
         Y = y(:,p(i:i+batch-1));
         X_num = size(X,4);
         idx=idx+1;
+        
+        % momentum enable
+        if i == 20*150+1
+            alpha_m = 0.95;
+        end
         
         dU1 = zeros(size(U1));% a*b
         dU2 = zeros(size(U2));% c*d
@@ -108,14 +120,10 @@ for e = 1:epo
         % softmax error
         out = exp(out_layer)./sum(exp(out_layer));
         wCost = lambda/2 * (sum(U3(:).^2)+sum(U2(:).^2)+sum(U1(:).^2));
-        error(idx*e,:) = sum(-sum(Y.*log(out)))/batch + wCost;
-        %% nes Momentum
-        U3 = U3 + alpha_m*v3;
-        U2 = U2 + alpha_m*v2;
-        U1 = U1 + alpha_m*v1;
+        error(idx,:) = sum(-sum(Y.*log(out)))/batch + wCost;
         %% Backpropagation
-        gradient3 =  out - Y; %out error gradient
-        
+        gradient3 = Y - out; %out error gradient
+
         % o*p / CONV - FOOL - FC
         gradient_FC2 = reshape(U3' * gradient3,layerDim2,layerDim2,kernelSize2(4),X_num);
         gradient2 = UpSampling(gradient_FC2,poolDim2,pool_idx2).*ReluGradient(z2);
@@ -128,32 +136,36 @@ for e = 1:epo
         dB3 = sum(gradient3,2);
         [dU2,dB2] = Update_grad(dU2,dB2,pool_layer1,gradient2);
         [dU1,dB1] = Update_grad(dU1,dB1,X,gradient1);
-
-         
+                
         %% Momentum
-        v3 = alpha_m*v3 - lr*(dU3/batch+lambda*U3);
-        vb3 = alpha_m*vb3 - lr*dB3/batch;
-        v2 = alpha_m*v2 - lr*(dU2/batch+lambda*U2);
-        vb2 = alpha_m*vb2 - lr*dB2/batch;
-        v1 = alpha_m*v1 - lr*(dU1/batch+lambda*U1);
-        vb1 = alpha_m*vb1 - lr*dB1/batch;
+        v3 = alpha_m*v3 + (1-alpha_m)*dU3/batch;v3 = v3/(1-(alpha_m)^idx);
+        vb3 = alpha_m*vb3 + (1-alpha_m)*dB3/batch;vb3 = vb3/(1-(alpha_m)^idx);
+        v2 = alpha_m*v2 + (1-alpha_m)*dU2/batch;v2 = v2/(1-(alpha_m)^idx);
+        vb2 = alpha_m*vb2 + (1-alpha_m)*dB2/batch;vb2 = vb2/(1-(alpha_m)^idx);
+        v1 = alpha_m*v1 + (1-alpha_m)*dU1/batch;v1 = v1/(1-(alpha_m)^idx);
+        vb1 = alpha_m*vb1 + (1-alpha_m)*dB1/batch;vb1 = vb1/(1-(alpha_m)^idx);
         
-        U3 = U3 + v3;
-        B3 = B3 + vb3;
-        U2 = U2 + v2;
-        B2 = B2 + vb2;
-        U1 = U1 + v1;
-        B1 = B1 + vb1;
-        
+        %% RMSProp
+        r3 =  alpha_r*r3 + (1-alpha_r)*(dU3/batch).^2;r3 = r3/(1-(alpha_r)^idx);
+        rb3 =  alpha_r*rb3 + (1-alpha_r)*(dB3/batch).^2;rb3 = rb3/(1-(alpha_r)^idx);
+        r2 =  alpha_r*r2 + (1-alpha_r)*(dU2/batch).^2;r2 = r2/(1-(alpha_r)^idx);
+        rb2 =  alpha_r*rb2 + (1-alpha_r)*(dB2/batch).^2;rb2 = rb2/(1-(alpha_r)^idx);
+        r1 =  alpha_r*r1 + (1-alpha_r)*(dU1/batch).^2;r1 = r1/(1-(alpha_r)^idx);
+        rb1 =  alpha_r*rb1 + (1- alpha_r)*(dB1/batch).^2;rb1 = rb1/(1-(alpha_r)^idx);
+              
+        U3 = U3 - lr*(v3./((1e-4) + sqrt(r3)) + lambda*U3);
+        B3 = B3 - lr*vb3./((1e-4) + sqrt(rb3));
+        U2 = U2 - lr*(v2./((1e-4) + sqrt(r2)) + lambda*U2);
+        B2 = B2 - lr*vb2./((1e-4) + sqrt(rb2));
+        U1 = U1 - lr*(v1./((1e-4) + sqrt(r1)) + lambda*U1);
+        B1 = B1 - lr*vb1./((1e-4) + sqrt(rb1));
+               
         tex1 = mean(error);
-        fprintf("%2.0f epoch 진행도 : %2.4f %% 전체 학습 오차: %0.4f\n",e,i/length(x)*100,round(tex1,4))
+        fprintf("%2.0f epoch 진행도 : %2.4f %% 전체 학습 오차: %0.4f\n",e,i/length(x)*100,error(idx))
     end
-    lr = lr/2;
     time = toc;
 
     testim = reshape(images, [28,28,1,10000]);
-    one_hot = diag(ones(1,max(labels)+1));
-    testlabel = one_hot(labels+1,:)';
     
     z1 = Correlation(testim,U1,B1);
     z1 = Normalization3(z1);
@@ -180,7 +192,6 @@ for e = 1:epo
     plot(smoothdata(error),'r','LineWidth',1)
   
 end
-
 
 
 
