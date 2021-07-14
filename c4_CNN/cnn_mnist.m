39cb36d1cc1b5ecb32f878617440d89300a84d1f
qntjrrud11 <qntjrrud11@naver.com>
2021-07-14 15:53:10 +0900

    [21.07.14] mesterov momentum / error 1.1 test_acc 80%

--- c4_CNN/cnn_mnist.m
+++ c4_CNN/cnn_mnist.m
@@ -40,10 +40,9 @@ kernelSize3 = [labelClasses layerDim3];
 
 % 학습률
 lr = 0.01;
-% 가중치 감쇠
+%weight decay
 lambda = 0.0001;
-% RMSProp
-alpha_r = 0.9;
+alpha_m = 0.9;
 
 %세대 수 (epoch)
 epo = 5;
@@ -59,12 +58,12 @@ B1 = zeros(kernelSize1(4), 1);
 B2 = zeros(kernelSize2(4), 1);
 B3 = zeros(labelClasses, 1);
 %momentum
-r1 = zeros(size(U1));% a*b
-r2 = zeros(size(U2));% c*d
-r3 = zeros(size(U3));% r*q
-rb1 = zeros(size(B1));
-rb2 = zeros(size(B2));
-rb3 = zeros(size(B3));
+v1 = zeros(size(U1));% a*b
+v2 = zeros(size(U2));% c*d
+v3 = zeros(size(U3));% r*q
+vb1 = zeros(size(B1));
+vb2 = zeros(size(B2));
+vb3 = zeros(size(B3));
 
 tic
 %%
@@ -91,6 +90,7 @@ for e = 1:epo
         % 입력데이터 정규화
 %         X =  Relu(Normalization3(X));
         X = X/255;
+        
         z1 = Correlation(X,U1,B1);
         z1 = Normalization3(z1);
         layer1 = Relu(z1);% m*n
@@ -109,9 +109,12 @@ for e = 1:epo
         out = exp(out_layer)./sum(exp(out_layer));
         wCost = lambda/2 * (sum(U3(:).^2)+sum(U2(:).^2)+sum(U1(:).^2));
         error(idx*e,:) = sum(-sum(Y.*log(out)))/batch + wCost;
-
+        %% nes Momentum
+        U3 = U3 + alpha_m*v3;
+        U2 = U2 + alpha_m*v2;
+        U1 = U1 + alpha_m*v1;
         %% Backpropagation
-        gradient3 = out - Y; %out error gradient
+        gradient3 =  out - Y; %out error gradient
         
         % o*p / CONV - FOOL - FC
         gradient_FC2 = reshape(U3' * gradient3,layerDim2,layerDim2,kernelSize2(4),X_num);
@@ -121,30 +124,29 @@ for e = 1:epo
         gradient_CONV1 = Convolution(gradient2,U2);
         gradient1 = UpSampling(gradient_CONV1,poolDim1,pool_idx1).*ReluGradient(z1);
         
-        % 누적 그레디언트
         dU3 = gradient3*flat_layer3';
         dB3 = sum(gradient3,2);
         [dU2,dB2] = Update_grad(dU2,dB2,pool_layer1,gradient2);
         [dU1,dB1] = Update_grad(dU1,dB1,X,gradient1);
 
          
-        %% RMSProp
-        r3 =  alpha_r*r3 +(1-alpha_r)*(dU3/batch+lambda*U3).^2;
-        rb3 =  alpha_r*rb3 +(1-alpha_r)*(dB3/batch).^2;
-        r2 =  alpha_r*r2 +(1-alpha_r)*(dU2/batch+lambda*U2).^2;
-        rb2 =  alpha_r*rb2 +(1-alpha_r)*(dB2/batch).^2;
-        r1 =  alpha_r*r1 +(1-alpha_r)*(dU1/batch+lambda*U1).^2;
-        rb1 =  alpha_r*rb1 + (1- alpha_r)*(dB1/batch).^2;
-              
-        U3 = U3 - (lr./(1e-4+sqrt(r3))).*(dU3/batch+lambda*U3);
-        B3 = B3 - (lr./(1e-4+sqrt(rb3))).*dB3/batch;
-        U2 = U2 - (lr./(1e-4+sqrt(r2))).*(dU2/batch+lambda*U2);
-        B2 = B2 - (lr./(1e-4+sqrt(rb2))).*dB2/batch;
-        U1 = U1 - (lr./(1e-4+sqrt(r1))).*(dU1/batch+lambda*U1);
-        B1 = B1 - (lr./(1e-4+sqrt(rb1))).*dB1/batch;
+        %% Momentum
+        v3 = alpha_m*v3 - lr*(dU3/batch+lambda*U3);
+        vb3 = alpha_m*vb3 - lr*dB3/batch;
+        v2 = alpha_m*v2 - lr*(dU2/batch+lambda*U2);
+        vb2 = alpha_m*vb2 - lr*dB2/batch;
+        v1 = alpha_m*v1 - lr*(dU1/batch+lambda*U1);
+        vb1 = alpha_m*vb1 - lr*dB1/batch;
+        
+        U3 = U3 + v3;
+        B3 = B3 + vb3;
+        U2 = U2 + v2;
+        B2 = B2 + vb2;
+        U1 = U1 + v1;
+        B1 = B1 + vb1;
         
         tex1 = mean(error);
-        fprintf("%2.0f epoch / 진행도 : %2.4f %% 전체 학습 오차: %0.4f\n",e,i/length(x)*100,round(tex1,4))
+        fprintf("%2.0f epoch 진행도 : %2.4f %% 전체 학습 오차: %0.4f\n",e,i/length(x)*100,round(tex1,4))
     end
     lr = lr/2;
     time = toc;
@@ -172,9 +174,9 @@ for e = 1:epo
     [~,preds] = max(out,[],1);
     
     acc = sum((preds-1)==labels)/length(preds);
-    fprintf('%2.0f epoch / Accuracy is %4.2f %%\n',e,acc*100);
+      fprintf('%2.0f epoch / Accuracy is %4.2f %%\n',e,acc*100);
     time
-    plot(error,'y');hold on
+     plot(error,'y');hold on
     plot(smoothdata(error),'r','LineWidth',1)
   
 end
