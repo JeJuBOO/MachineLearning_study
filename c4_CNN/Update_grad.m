698aba04d4ad012d3383e4117f817348bde3e426
qntjrrud11 <qntjrrud11@naver.com>
2021-07-15 14:15:37 +0900

    .

--- c4_CNN/Update_grad.m
+++ c4_CNN/Update_grad.m
@@ -13,7 +13,6 @@ for n = 1:u_n
     for ch = 1:u_ch 
         for d = 1:l_n 
             dU(:,:,ch,n) = dU(:,:,ch,n) + conv2(layer(:,:,ch,d),rot90(grad(:,:,n,d),2),'valid');
-%             dU(:,:,ch,n) = dU(:,:,ch,n) + filter2(grad(:,:,n,d),layer(:,:,ch,d),'valid');
         end
         dB(n) = sum(grad(:,:,n,:),'all');
     end
