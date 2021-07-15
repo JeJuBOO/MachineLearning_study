49f6e696ee7e565be22b31d795fa62b59ca9c31b
qntjrrud11 <qntjrrud11@naver.com>
2021-07-15 14:16:29 +0900

    [21.07.15] max good

--- c4_CNN/UpSampling.m
+++ c4_CNN/UpSampling.m
@@ -1,21 +1,21 @@
 % 21.06.29 sk.boo
 % 풀링레이어의 미분
-function out = UpSampling(in,pool_size,pool_idx,method)
+function out = UpSampling(in,pool_size,method,pool_grad)
 arguments
     in (:,:,:,:) double % 풀링레이어 전의 행렬
     pool_size (1,1) double % 풀링사이즈
-    pool_idx (:,:,:,:) double
-    method = "max"
+    method = "mean"
+    pool_grad (:,:,:,:) double = 0
+
 end
 
 [col,row,ch,num] = size(in);
 out = zeros(col*pool_size,row*pool_size,ch,num);
-pool = zeros([pool_size pool_size ch num]);
 if method == "max"
-    for o = 1:col
-        for p = 1:row
-            pool(pool_idx(o,p,:,:)) = in(o,p,:,:);
-            out((o-1)*pool_size+1:o*pool_size,(p-1)*pool_size+1:p*pool_size,:,:) = pool;
+    for n = 1:num
+        for c = 1:ch
+            unpool = in(:,:,c,n);
+            out(:,:,c,n) = kron(unpool,ones(pool_size)).*pool_grad(:,:,c,n);
         end
     end
 elseif method == "mean"
