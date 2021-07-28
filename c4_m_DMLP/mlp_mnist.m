71bd28c81520d5e6d051e5b6502c672e159c2845
qntjrrud11 <qntjrrud11@naver.com>
2021-07-28 11:31:41 +0900

    [21.07.28] mlp 4layer

--- c4_m_DMLP/mlp_mnist.m
+++ c4_m_DMLP/mlp_mnist.m
@@ -7,24 +7,23 @@ clc,clear,close all
 % sk.boo
 
 % training set (MNIST)
-% load mnist\trainingData.mat;
-% X = reshape(images,28^2,[]);% input : 28^2 x 60000
-% one_hot = diag(ones(1,max(labels)+1));
-% Y = one_hot(labels+1,:)';% output : 10 x 60000
-% X = [ones(1,length(X)) ; X]; % bias add
-% X = (X-mean(X))./std(X);
+load mnist\trainingData.mat;
+X = reshape(images,28^2,[]);% input : 28^2 x 60000
+one_hot = diag(ones(1,max(labels)+1));
+Y = one_hot(labels+1,:)';% output : 10 x 60000
+X = X/255;
 
-load iris.dat
-X = iris([1:40, 51:90, 101:140],1:4)';
-Y = iris([1:40, 51:90, 101:140],5)';
-one_hot = diag(ones(1,max(Y)));
-Y = one_hot(Y,:)';% output : 10 x 60000
-X = (X-mean(X))./std(X);
+% load iris.dat
+% X = iris([1:40, 51:90, 101:140],1:4)';
+% Y = iris([1:40, 51:90, 101:140],5)';
+% one_hot = diag(ones(1,max(Y)));
+% Y = one_hot(Y,:)';% output : 10 x 60000
+% X = (X-mean(X))./std(X);
 
 %number of nodes
 in_node_n = length(X(:,1));
 hd_node_n = 15;
-hd2_node_n = 10;
+hd2_node_n = 12;
 hd3_node_n = 10;
 out_node_n = length(Y(:,1));
 
@@ -36,10 +35,13 @@ ep = 1;
 
 %weight matrix
 U1 = randn(hd_node_n,in_node_n);
-U2 = randn(hd2_node_n,hd_node_n+1);
-U3 = randn(hd3_node_n,hd2_node_n+1);
-U4 = randn(out_node_n,hd3_node_n+1);
-
+U2 = randn(hd2_node_n,hd_node_n);
+U3 = randn(hd3_node_n,hd2_node_n);
+U4 = randn(out_node_n,hd3_node_n);
+B1 = zeros(hd_node_n,1);
+B2 = zeros(hd2_node_n,1);
+B3 = zeros(hd3_node_n,1);
+B4 = zeros(out_node_n,1);
 %momentum
 v1 = 0;v2 = 0;v3 = 0;v4 = 0;
 r1 = 0;r2 = 0;r3 = 0;r4 = 0;
@@ -47,101 +49,109 @@ r1 = 0;r2 = 0;r3 = 0;r4 = 0;
 
 tic
 epo = 5000;
-sample_n = 100;
-for j=1:epo
-    sample_index = randperm(length(X));
-    X_sample = X(:,sample_index(1:sample_n));
-    Y_sample = Y(:,sample_index(1:sample_n));
+batchSize = 100;
+idx = 0;
+for e = 1:epo
+    % data shuffle
+    p = randperm(length(X));
     
-    dU1 = zeros(size(U1));
-    dU2 = zeros(size(U2));
-    dU3 = zeros(size(U3));
-    dU4 = zeros(size(U4));
-    
-    for i=1:sample_n
-        %% forward
-        hidden_node = [1; Forward_mlp(@Relu,X_sample(:,i),U1)];
-        hidden_node = (hidden_node-mean(hidden_node))./std(hidden_node);
-        hidden2_node = [1; Forward_mlp(@Relu,hidden_node,U2)];
-        hidden2_node = (hidden2_node-mean(hidden2_node))./std(hidden2_node);
-        hidden3_node = [1; Forward_mlp(@Relu,hidden2_node,U3)];
-        hidden3_node = (hidden3_node-mean(hidden3_node))./std(hidden3_node);
-        o = Forward_mlp(@exp,hidden3_node,U4)/sum(Forward_mlp(@exp,hidden3_node,U4));
-        %% Error
-        o_num(i) = find(o==max(o));
-        y_num(i) = find(Y_sample(:,i)==1);
-        error(i,:) = -sum(Y_sample(:,i).*log(o));
+    for i = 1:batchSize:(length(X)-batchSize+1)
+        X_sample = X(:,p(i:i+batchSize-1));
+        Y_sample = Y(:,p(i:i+batchSize-1));
+        X_num = size(X_sample,4);
+        idx=idx+1;
         
-        %% Momentum
-        hU4 = U4 + alpha_m*v4;
-        hU3 = U3 + alpha_m*v3;
-        hU2 = U2 + alpha_m*v2;
-        hU1 = U1 + alpha_m*v1;
+        %% forward
+       
+        hidden_node = Relu(U1*X_sample+B1);
+        hidden2_node = Relu(U2*hidden_node+B2);
+        hidden3_node = Relu(U3*hidden2_node+B3);
+        hidden4 = U4*hidden3_node+B4;
+        
+        o = exp(hidden4)./sum(exp(hidden4),1);
+        %% Error
+%         o_num(i) = find(o==max(o));
+%         y_num(i) = find(Y_sample(:,i)==1);
+        error(idx,:) = -sum(sum(Y_sample.*log(o)))/batchSize;
         
         %% error backpropagation
-        gradient1 = (Y_sample(:,i) - o);
-        dU4 = dU4 -gradient1*hidden3_node';
-        gradient2 = (gradient1'*hU4(:,2:end))'.*Forward_mlp(@ReluGradient,hidden2_node,hU3);
-        dU3 = dU3 -gradient2*hidden2_node';
-        gradient3 = (gradient2'*hU3(:,2:end))'.*Forward_mlp(@ReluGradient,hidden_node,hU2);
-        dU2 = dU2 -gradient3*hidden_node';
-        gradient4 = (gradient3'*hU2(:,2:end))'.*Forward_mlp(@ReluGradient,X_sample(:,i),hU1);
-        dU1 = dU1 -gradient4*X_sample(:,i)';
+        gradient4 = (o - Y_sample);     
+        gradient3 = (gradient4'*U4)'.*ReluGradient(U3*hidden2_node);       
+        gradient2 = (gradient3'*U3)'.*ReluGradient(U2*hidden_node);
+        gradient1 = (gradient2'*U2)'.*ReluGradient(U1*X_sample);
+        
+        dU4 = (gradient4*hidden3_node')/batchSize;
+        dB4 = sum(gradient4,2)/batchSize;
+        dU3 = (gradient3*hidden2_node')/batchSize;
+        dB3 = sum(gradient3,2)/batchSize;
+        dU2 = (gradient2*hidden_node')/batchSize;
+        dB2 = sum(gradient2,2)/batchSize;
+        dU1 = (gradient1*X_sample')/batchSize;
+        dB1 = sum(gradient1,2)/batchSize;
+        
+%         %% Momentum
+%         v4 = alpha_m*v4 + (1-alpha_m)*dU4; v4 = v4/(1-(alpha_m)^idx);
+%         v3 = alpha_m*v3 + (1-alpha_m)*dU3; v3 = v3/(1-(alpha_m)^idx);
+%         v2 = alpha_m*v2 + (1-alpha_m)*dU2; v2 = v2/(1-(alpha_m)^idx);
+%         v1 = alpha_m*v1 + (1-alpha_m)*dU1; v1 = v1/(1-(alpha_m)^idx);
+%         %% RMSProp
+%         r4 = alpha_lr*r4 + (1-alpha_lr)*dU4.*dU4; r4 = r4/(1-(alpha_lr)^idx);
+%         r3 = alpha_lr*r3 + (1-alpha_lr)*dU3.*dU3; r3 = r3/(1-(alpha_lr)^idx);
+%         r2 = alpha_lr*r2 + (1-alpha_lr)*dU2.*dU2; r2 = r2/(1-(alpha_lr)^idx);
+%         r1 = alpha_lr*r1 + (1-alpha_lr)*dU1.*dU1; r1 = r1/(1-(alpha_lr)^idx);
+%         
+%         %% update weight
+%         U4 = U4 - (lr./((1e-10)+sqrt(r4))).*v4 - lr*ep*sign(U4);
+%         U3 = U3 - (lr./((1e-10)+sqrt(r3))).*v3 - lr*ep*sign(U3);
+%         U2 = U2 - (lr./((1e-10)+sqrt(r2))).*v2 - lr*ep*sign(U2);
+%         U1 = U1 - (lr./((1e-10)+sqrt(r1))).*v1 - lr*ep*sign(U1);
+        %% update weight
+        U4 = U4 - lr*dU4 - lr*ep*sign(U4);
+        B4 = B4 - lr*dB4;
+        U3 = U3 - lr*dU3 - lr*ep*sign(U3);
+        B3 = B3 - lr*dB3;
+        U2 = U2 - lr*dU2 - lr*ep*sign(U2);
+        B2 = B2 - lr*dB2;
+        U1 = U1 - lr*dU1 - lr*ep*sign(U1);
+        B1 = B1 - lr*dB1;
+        
+        fprintf("%2.0f epoch 진행도 : %2.4f %% 전체 학습 오차: %0.4f\n",e,i/length(X)*100,error(idx))
     end
-    g4 = dU4/sample_n;
-    g3 = dU3/sample_n;
-    g2 = dU2/sample_n;
-    g1 = dU1/sample_n;
-    
-    %% Momentum
-    v4 = alpha_m*v4 - (1-alpha_m)*g4; v4 = v4/(1-(alpha_m)^t);
-    v3 = alpha_m*v3 - (1-alpha_m)*g3; v3 = v3/(1-(alpha_m)^t);
-    v2 = alpha_m*v2 - (1-alpha_m)*g2; v2 = v2/(1-(alpha_m)^t);
-    v1 = alpha_m*v1 - (1-alpha_m)*g1; v1 = v1/(1-(alpha_m)^t);
-    %% RMSProp
-    r4 = alpha_lr*r4 + (1-alpha_lr)*g4.*g4; r4 = r4/(1-(alpha_lr)^t);
-    r3 = alpha_lr*r3 + (1-alpha_lr)*g3.*g3; r3 = r3/(1-(alpha_lr)^t);
-    r2 = alpha_lr*r2 + (1-alpha_lr)*g2.*g2; r2 = r2/(1-(alpha_lr)^t);
-    r1 = alpha_lr*r1 + (1-alpha_lr)*g1.*g1; r1 = r1/(1-(alpha_lr)^t);
-    
-    %% update weight
-    U4 = U4 - (lr./((1e-10)+sqrt(r4))).*v4 - lr*ep*sign(U4);
-    U3 = U3 - (lr./((1e-10)+sqrt(r3))).*v3 - lr*ep*sign(U3);
-    U2 = U2 - (lr./((1e-10)+sqrt(r2))).*v2 - lr*ep*sign(U2);
-    U1 = U1 - (lr./((1e-10)+sqrt(r1))).*v1 - lr*ep*sign(U1);
-    
-    clc
-    tex2 = mean(o_num == y_num);
-    tex1 = mean(error);
-    mse(j,1) = tex1;
-    fprintf("학습 횟수 : %d번\n",j)
-    fprintf("학습된 글자 수 : %d 개 (한 번 반복에 64개씩 학습을 진행합니다.)\n",j*sample_n)
-    fprintf("학습 데이터 손글씨 인식률 : %0.2f%%\n",round(tex2*100,4))
-    fprintf("전체 학습 오차(MSE) : %0.5f\n",round(tex1,4))
-    %     cla
-    %
-    %     plot(mse);
-    %     axis([0 inf 0 5])
-    %     title("MSE")
-    %     drawnow;
+%% test
+load mnist\testingData.mat;
+X_test = reshape(images,28^2,[]);% input : 28^2 x 10000
+one_hot = diag(ones(1,max(labels)+1));
+Y_test = one_hot(labels+1,:)';% output : 10 x 10000
+X_test = X_test/255;
+%% forward
+hidden_node = Relu(U1*X_test+B1);
+hidden2_node = Relu(U2*hidden_node+B2);
+hidden3_node = Relu(U3*hidden2_node+B3);
+hidden4 = U4*hidden3_node+B4;
+
+o = exp(hidden4)./sum(exp(hidden4),1);
+[~,preds] = max(o,[],1);
+
+acc = sum((preds-1)==labels)/length(preds);
+fprintf('%2.0f epoch / Accuracy is %4.2f %%\n',e,acc*100);
 end
 
 toc
 
 
 %% test
-% load mnist\testingData.mat;
-% X = reshape(images,28^2,[]);% input : 28^2 x 10000
-% one_hot = diag(ones(1,max(labels)+1));
-% Y = one_hot(labels+1,:)';% output : 10 x 10000
-% X = [ones(1,length(X)) ; X]; % bias add
-% X = (X-mean(X))./std(X);
-
-X = iris([41:50, 91:100, 141:150],1:4)';
-labels = iris([41:50, 91:100, 141:150],5)';
-one_hot = diag(ones(1,max(labels)));
-Y = one_hot(labels,:)';% output : 10 x 60000
+load mnist\testingData.mat;
+X = reshape(images,28^2,[]);% input : 28^2 x 10000
+one_hot = diag(ones(1,max(labels)+1));
+Y = one_hot(labels+1,:)';% output : 10 x 10000
+X = [ones(1,length(X)) ; X]; % bias add
 X = (X-mean(X))./std(X);
+
+% X = iris([41:50, 91:100, 141:150],1:4)';
+% labels = iris([41:50, 91:100, 141:150],5)';
+% one_hot = diag(ones(1,max(labels)));
+% Y = one_hot(labels,:)';% output : 10 x 60000
+% X = (X-mean(X))./std(X);
 
 o = zeros(size(Y));
 fail = 0;
@@ -171,5 +181,8 @@ fprintf("test 집합에서 틀린 개수 : %d\n",fail);
 %forward
 function z = Forward_mlp(act,x,U)
 z = act(U*x);
+end
+function z = Normalization(x)
+z = (x-mean(x))./std(x);
 end
 
